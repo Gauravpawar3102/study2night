661cd70c28b8793a380ed39c64587e7aed57f411
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-08-18 13:09:37 +0530

    basic testing functionality completed

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 function Courses() {
+  const [data, setData] = useState([]);
   const YOUTUBE_PLAYLIST_ITEMS_API =
     'https://www.googleapis.com/youtube/v3/playlistItems';
 
-  return <div className="container"></div>;
+  useEffect(() => {
+    fetchApi();
+  }, []);
+  // GET https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=UUpRmvjdu3ixew5ahydZ67uA&key={YOUR_API_KEY}
+
+  async function fetchApi() {
+    const response = await fetch(
+      `${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&maxResults=50&playlistId=UUpRmvjdu3ixew5ahydZ67uA&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+    );
+    const apiData = await response.json();
+    setData(apiData.items);
+    console.log(data);
+  }
+
+  return (
+    <div className="container">
+      {data.map((dataItem) => {
+        console.log('item', dataItem);
+        return (
+          <div className="">
+            <div className="">{dataItem.snippet.title}</div>
+            <div className="">
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${dataItem.snippet.resourceId.videoId}`}
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Courses;
